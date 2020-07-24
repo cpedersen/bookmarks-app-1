3b2d2a990f8d09b109d97e71cabc3f43c9e51f4b
Catherine Pedersen <cathy.pedersen@gmail.com>
2020-07-24 19:19:10 -0400

    Add accessibility improvements

--- src/Rating/Rating.js
+++ src/Rating/Rating.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Rating.css';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 
 export default function Rating(props) {
   const stars = [0, 0, 0, 0, 0].map((_, i) =>
