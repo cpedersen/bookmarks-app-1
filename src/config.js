b190854f898ab6cf2b1dd190aa4e16c9408d9803
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-01-14 16:46:01 -0500

    Committing latest files

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
   //API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
+  //API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
+  API_ENDPOINT: `postgres://wqaygcrqxqwmcn:bf0f246919b6fad880946fd63d4ed73373697e7ee4eb930e986c6b84f88b1146@ec2-18-232-232-96.compute-1.amazonaws.com:5432/dbj3vbde6m7a5d`,
   API_KEY: process.env.REACT_APP_API_KEY
 }
